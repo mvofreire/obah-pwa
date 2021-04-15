dbae6b18ae5453e9e0620f46b9ecf25baa2949bb
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-15 18:29:47 -0300

    Update index.ts
    remove unused variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,3 @@
-import { Envs } from "./envs";
-
-const env = process.env.NODE_ENV || Envs.development;
-const isProd = env === Envs.production;
 const baseUrl = process.env.REACT_APP_API_URL;
 
 const config = {
