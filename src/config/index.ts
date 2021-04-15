f5ecf6817abcb060c49c2b2d2dfc9ff93120f435
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-15 09:28:16 -0300

    log env

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,7 @@ const isProd = env === "production";
 const baseUrl = isProd
   ? process.env.REACT_APP_API_URL
   : process.env.REACT_APP_API_DEV_URL;
-
 console.log(env, baseUrl, isProd);
-
 const config = {
   storageKey: "obah-key",
   storageUserKey: "obah-user-key",
