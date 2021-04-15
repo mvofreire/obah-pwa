d6e9fd9dedcfdf9438625ff4ccaa479177e4ad81
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-15 18:27:16 -0300

    Update index.ts
    using only REACT_APP_API_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,7 @@ import { Envs } from "./envs";
 
 const env = process.env.NODE_ENV || Envs.development;
 const isProd = env === Envs.production;
-
-const baseUrl = isProd
-  ? process.env.REACT_APP_API_URL
-  : process.env.REACT_APP_API_DEV_URL;
-
-console.log(env, baseUrl, process.env.VERCEL, 'dev');
+const baseUrl = process.env.REACT_APP_API_URL;
 
 const config = {
   storageKey: "obah-key",
