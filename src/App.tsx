5fa3fdec92fced8888b194a31d534fcf6c09a388
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-19 11:19:50 -0300

    add update message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, notification } from "antd";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { AppContextProvider, useAppContext } from "contexts/app.context";
 import { ThemeProvider } from "@material-ui/styles";
@@ -7,6 +7,7 @@ import PrivateContainer from "containers/private";
 import PublicContainer from "containers/public";
 import theme from "./theme-variables";
 import "./App.less";
+import { useCallback, useEffect } from "react";
 
 const queryClient = new QueryClient();
 function AppContainer() {
@@ -27,6 +28,33 @@ function AppContainer() {
 
 function App() {
   const { session } = useAppContext();
+
+  const handleNewVersionAvaiableEvent = useCallback(() => {
+    console.log("show NewVersionAvaiableEvent");
+    notification.info({
+      message: "Existe uma nova versão do App",
+      duration: null,
+      onClick() {
+        const href = window.location.href;
+        window.location.href = href;
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener(
+      "NewVersionAvaiable",
+      handleNewVersionAvaiableEvent
+    );
+
+    return () => {
+      window.removeEventListener(
+        "NewVersionAvaiable",
+        handleNewVersionAvaiableEvent
+      );
+    };
+  }, [handleNewVersionAvaiableEvent]);
+
   return (
     <>
       {!!session && <PrivateContainer />}
